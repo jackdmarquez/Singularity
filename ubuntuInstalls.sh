e52b6786f921437fb770aa87f7c8b9c35fe2257a
jackdmarquez <jackdmarquez@gmail.com>
2019-03-20 10:37:33 -0500

    Update ubuntuInstalls.sh

--- ubuntuInstalls.sh
+++ ubuntuInstalls.sh
@@ -30,7 +30,7 @@ echo "Installing Singularity........... 62%"
 sudo apt-get update && \
 sudo apt-get install -y build-essential \
 libssl-dev uuid-dev libgpgme11-dev libseccomp-dev pkg-config squashfs-tools
-export VERSION=1.11.4 OS=linux ARCH=amd64
+export VERSION=1.11.5 OS=linux ARCH=amd64
 echo "Installing GO........... 74% "
 wget -O /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.ta$
 sudo tar -C /usr/local -xzf /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
@@ -42,6 +42,11 @@ mkdir -p ${GOPATH}/src/github.com/sylabs && \
 cd ${GOPATH}/src/github.com/sylabs && \
 git clone https://github.com/sylabs/singularity.git && \
 cd singularity
+cd ${GOPATH}/src/github.com/sylabs/singularity && \
+  ./mconfig && \
+  cd ./builddir && \
+  make && \
+  sudo make install
 git checkout v3.1.0
 singularity version
-echo "Installation Complete........... 100%"
+echo "If there were no errors, consider it done........... 100%"
